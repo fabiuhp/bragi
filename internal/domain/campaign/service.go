6b77a86543f7b7765e7fc2c55ab248da65d92516
Fabio Pereira <fabiuhp@msn.com>
2024-07-06 17:00:55 -0300

    dominio, testes

---
+++ internal/domain/campaign/service.go
@@ -0,0 +1,18 @@
+package campaign
+
+import (
+	"bragi/internal/contract"
+)
+
+type Service struct {
+	Repository Repository
+}
+
+func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	if err != nil {
+		return "", err
+	}
+	s.Repository.Save(campaign)
+	return campaign.ID, nil
+}
---
+++ internal/domain/campaign/service_test.go
@@ -0,0 +1,64 @@
+package campaign
+
+import (
+	"bragi/internal/contract"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type repositoryMock struct {
+	mock.Mock
+}
+
+func (r *repositoryMock) Save(campaign *Campaign) error {
+	args := r.Called(campaign)
+	return args.Error(0)
+}
+
+var (
+	newCampaign = contract.NewCampaign{
+		Name:    "Teste A",
+		Content: "Corpo",
+		Emails:  []string{"fabio@email.com"},
+	}
+)
+
+func Test_Create_Campaign(t *testing.T) {
+	assert := assert.New(t)
+	service := Service{}
+
+	id, err := service.Create(newCampaign)
+
+	assert.NotNil(id)
+	assert.Nil(err)
+}
+
+func Test_Create_SaveCampaign(t *testing.T) {
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
+		if campaign.Name != newCampaign.Name ||
+			campaign.Content != newCampaign.Content ||
+			len(campaign.Contacts) != len(newCampaign.Emails) {
+			return false
+		}
+		return true
+	})).Return(nil)
+
+	service := Service{Repository: repositoryMock}
+	service.Create(newCampaign)
+	repositoryMock.AssertExpectations(t)
+}
+
+func Test_Create_ValidateDomainError(t *testing.T) {
+	assert := assert.New(t)
+	newCampaign.Name = ""
+
+	repositoryMock := new(repositoryMock)
+	service := Service{Repository: repositoryMock}
+	_, err := service.Create(newCampaign)
+
+	assert.NotNil(err)
+	assert.Equal("nome é obrigatório", err.Error())
+}
